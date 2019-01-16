49abd5c7f549301e5b7f31a8158d9c053751c00f
Lyioh <bidegaray.lionel@mail.com>
2019-01-16 16:33:21 +0100

    Flip DOne

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+const gmailLink = document.querySelector("#gmailLink");
+const gmailIcone = document.querySelector("#gmailIcone");
+const flipContainer = document.querySelector(".flip-container-inner");
+const frontContainer = document.querySelector(".flip-container-front");
+
 function copyAdress() {
   let tempElement = document.createElement("textarea");
   tempElement.value = "bidegaray.lionel@gmail.com";
@@ -6,14 +11,29 @@ function copyAdress() {
   document.execCommand("copy");
   document.body.removeChild(tempElement);
 
-  document.querySelector("#gmailIcone").style.display = "none";
+  gmailIcone.style.display = "none";
   let tempText = document.createElement("p");
   tempText.style.whiteSpace = "pre-wrap";
   tempText.innerHTML = "Email\nCopied";
-  document.querySelector("#gmailLink").appendChild(tempText);
+  gmailLink.appendChild(tempText);
 
   setTimeout(function removeTempText() {
-    document.querySelector("#gmailLink").removeChild(tempText);
-    document.querySelector("#gmailIcone").style.display = "inline";
+    gmailLink.removeChild(tempText);
+    gmailIcone.style.display = "inline";
   }, 500);
 }
+
+function flipTheCard() {
+  flipContainer.classList.toggle("is-flipped");
+  setTimeout(function() {
+    frontContainer.style.display = "none";
+  }, 800);
+}
+
+function flipTheCardBack() {
+  frontContainer.style.display = "block";
+  flipContainer.classList.toggle("is-flipped");
+}
+
+document.querySelector("#flip").addEventListener("click", flipTheCard);
+document.querySelector("#flip-back").addEventListener("click", flipTheCardBack);
